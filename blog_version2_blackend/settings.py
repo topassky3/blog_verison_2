c3b396d0ebb64a0670793d0c839b923c659f75ff
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-02-21 11:11:28 -0500

    Se agrego el msanej de eoror en español y mejor

--- blog_version2_blackend/settings.py
+++ blog_version2_blackend/settings.py
@@ -137,3 +137,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+LANGUAGE_CODE = 'es'  # Establece el idioma predeterminado en español
+USE_I18N = True       # Habilita la internacionalización
+USE_L10N = True       # Habilita la localización
--- registrarse/views.py
+++ registrarse/views.py
@@ -1,24 +1,39 @@
 # registrarse/views.py
-
 from django.views.generic import CreateView
 from registrarse.forms import LectorCreationForm
+
 
 class RegistrarseView(CreateView):
     form_class = LectorCreationForm
     template_name = "registrarse/registrarse.html"
 
     def form_valid(self, form):
-        # Se guarda el usuario
         self.object = form.save()
-        # Preparamos el contexto para el mensaje de éxito
         context = self.get_context_data(form=form)
         context['alert_message'] = "Registro exitoso. Ahora puedes iniciar sesión."
-        context['redirect_url'] = "/login"  # Aquí pones la URL de inicio
+        context['redirect_url'] = "/login"  # URL a la que se redirige en caso de éxito
         return self.render_to_response(context)
 
     def form_invalid(self, form):
-        # Preparamos el contexto para el mensaje de error
+        # Mapeo de nombres de campos a etiquetas en español
+        field_labels = {
+            "fullname": "Nombre Completo",
+            "email": "Correo Electrónico",
+            "password1": "Contraseña",
+            "password2": "Confirmar Contraseña",
+            "__all__": ""
+        }
+        error_list = []
+        for field, errors in form.errors.items():
+            label = field_labels.get(field, field)
+            for error in errors:
+                if label:
+                    error_list.append(f"{label}: {error}")
+                else:
+                    error_list.append(f"{error}")
+        error_message = "Se encontraron los siguientes errores:\n" + "\n".join(error_list)
+
         context = self.get_context_data(form=form)
-        context['alert_message'] = "Hubo un error durante el registro. Por favor, revisa la información e inténtalo de nuevo."
-        context['redirect_url'] = "/registrarse/"  # O la URL que desees para volver a intentar
+        context['alert_message'] = error_message
+        context['redirect_url'] = "/registrarse/"  # Redirige de nuevo a la página de registro en caso de error
         return self.render_to_response(context)
