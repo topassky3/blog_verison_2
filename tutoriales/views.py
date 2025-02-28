723982152b5be4c11e45c166f9b396d870e9d226
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-02-28 14:32:31 -0500

    Ya se peude cargar los tutorial es publicados

--- tutoriales/views.py
+++ tutoriales/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from core.models import Tutorial
 
 class TutorialesView(TemplateView):
     template_name = "tutoriales/tutoriales.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Filtra solo los tutoriales publicados, ordenándolos (por ejemplo, del más reciente al más antiguo)
+        context['tutorials'] = Tutorial.objects.filter(publicado=True).order_by('-created_at')
+        return context
