2c750520830dc8c5f6074b9387abfefd43b90b99
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-03-05 09:42:53 -0500

    Se agrego la opcion para que el suario peuda borra rlso datos

--- podcast/urls.py
+++ podcast/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PodcastListView
+from .views import PodcastListView, PodcastDeleteView
 
 urlpatterns = [
     path('', PodcastListView.as_view(), name='podcast_home'),
+    path('eliminar/<int:pk>/', PodcastDeleteView.as_view(), name='podcast_delete'),
 ]
--- podcast/views.py
+++ podcast/views.py
@@ -19,3 +19,20 @@ class PodcastListView(ListView):
         # Agrega las categorías específicas de podcasts
         context['categories'] = PodcastCategory.objects.all().order_by('name')
         return context
+
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+from django.shortcuts import redirect
+from core.models import Podcast
+
+class PodcastDeleteView(DeleteView):
+    model = Podcast
+    template_name = 'crear_podcast/podcast_confirm_delete.html'
+    success_url = reverse_lazy('profile')  # O 'escritor_profile', según convenga
+
+    def dispatch(self, request, *args, **kwargs):
+        podcast = self.get_object()
+        # Solo el autor puede borrar su podcast
+        if not request.user.is_authenticated or request.user != podcast.author:
+            return redirect('login')
+        return super().dispatch(request, *args, **kwargs)
