96a11494e2f284c8d1534db2a3442bcdcefe34be
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-03-05 15:43:05 -0500

    Se agrego los coemtnairios a lso podcast

--- escuchar_podcast/urls.py
+++ escuchar_podcast/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import PodcastDetailView
+from .views import (
+    PodcastDetailView,
+    toggle_podcast_comment_like,
+    toggle_podcast_comment_dislike,
+    delete_podcast_comment
+)
 
 urlpatterns = [
     path('<int:pk>/', PodcastDetailView.as_view(), name='escuchar_podcast'),
+    path('toggle_comment_like/', toggle_podcast_comment_like, name='toggle_podcast_comment_like'),
+    path('toggle_comment_dislike/', toggle_podcast_comment_dislike, name='toggle_podcast_comment_dislike'),
+    path('delete_comment/', delete_podcast_comment, name='delete_podcast_comment'),
 ]
--- escuchar_podcast/views.py
+++ escuchar_podcast/views.py
@@ -43,6 +43,15 @@ class PodcastDetailView(FormMixin, DetailView):
             comment = form.save(commit=False)
             comment.podcast = self.object  # Asigna el podcast actual
             comment.author = request.user
+            # Verifica si se envió un comentario padre
+            parent_id = request.POST.get('parent')
+            if parent_id:
+                try:
+                    from core.models import PodcastComment
+                    parent_comment = PodcastComment.objects.get(id=parent_id)
+                    comment.parent = parent_comment
+                except PodcastComment.DoesNotExist:
+                    pass
             comment.save()
             return self.form_valid(form)
         else:
@@ -50,3 +59,74 @@ class PodcastDetailView(FormMixin, DetailView):
 
     def get_success_url(self):
         return reverse('escuchar_podcast', kwargs={'pk': self.object.pk}) + "#comments"
+
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from core.models import PodcastComment
+
+@login_required
+@require_POST
+def toggle_podcast_comment_like(request):
+    comment_id = request.POST.get('comment_id')
+    try:
+        comment = PodcastComment.objects.get(id=comment_id)
+    except PodcastComment.DoesNotExist:
+        return JsonResponse({'error': 'Comentario no encontrado.'}, status=404)
+
+    user = request.user
+    if user in comment.likes.all():
+        comment.likes.remove(user)
+        liked = False
+    else:
+        # Si el usuario ya dio "no me gusta", se quita
+        if user in comment.dislikes.all():
+            comment.dislikes.remove(user)
+        comment.likes.add(user)
+        liked = True
+
+    data = {
+        'liked': liked,
+        'like_count': comment.likes.count(),
+        'dislike_count': comment.dislikes.count()
+    }
+    return JsonResponse(data)
+
+@login_required
+@require_POST
+def toggle_podcast_comment_dislike(request):
+    comment_id = request.POST.get('comment_id')
+    try:
+        comment = PodcastComment.objects.get(id=comment_id)
+    except PodcastComment.DoesNotExist:
+        return JsonResponse({'error': 'Comentario no encontrado.'}, status=404)
+
+    user = request.user
+    if user in comment.dislikes.all():
+        comment.dislikes.remove(user)
+        disliked = False
+    else:
+        # Si el usuario ya dio "me gusta", se quita
+        if user in comment.likes.all():
+            comment.likes.remove(user)
+        comment.dislikes.add(user)
+        disliked = True
+
+    data = {
+        'disliked': disliked,
+        'dislike_count': comment.dislikes.count(),
+        'like_count': comment.likes.count()
+    }
+    return JsonResponse(data)
+
+@login_required
+@require_POST
+def delete_podcast_comment(request):
+    comment_id = request.POST.get('comment_id')
+    try:
+        comment = PodcastComment.objects.get(id=comment_id, author=request.user)
+        comment.delete()
+    except PodcastComment.DoesNotExist:
+        return JsonResponse({'error': 'Comentario no encontrado o no tienes permiso para borrarlo.'}, status=404)
+
+    return JsonResponse({'success': True})
