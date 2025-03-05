7cff77ea946effdb7691809e1a8f190bee31b913
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-03-05 10:02:02 -0500

    Se agrego als categorias a la aprte de crear el podast

--- crear_podcast/forms.py
+++ crear_podcast/forms.py
@@ -4,4 +4,4 @@ from core.models import Podcast
 class PodcastForm(forms.ModelForm):
     class Meta:
         model = Podcast
-        fields = ['title', 'description', 'audio', 'cover']
+        fields = ['title', 'description', 'audio', 'cover', 'category']
