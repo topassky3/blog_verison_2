6f5b2f5313485f6e9eb10ab2be51b73c4f1cc5b4
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-03-07 15:25:30 -0500

    Se agrego bton de plan basico actual

--- suscripcion/views.py
+++ suscripcion/views.py
@@ -1,4 +1,17 @@
 from django.views.generic import TemplateView
+from core.models import Subscription
+
 
 class SuscripcionView(TemplateView):
     template_name = "suscripcion/suscripcion.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            try:
+                context['subscription'] = Subscription.objects.get(user=self.request.user)
+            except Subscription.DoesNotExist:
+                context['subscription'] = None
+        else:
+            context['subscription'] = None
+        return context
