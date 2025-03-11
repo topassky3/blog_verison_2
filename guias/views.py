4992fafd3227baa49d80acd18c6140249c408be9
Juan Felipe Orozco <orozjuanfelipe@gmail.com>
2025-03-11 18:46:48 -0500

    Se logro un objetivo

--- guias/views.py
+++ guias/views.py
@@ -2,14 +2,28 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import TemplateView
 from core.models import Guia, GuiaCategory
 
+
 class GuiasView(TemplateView):
     template_name = "guias/guias.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['categories'] = GuiaCategory.objects.all()
+
+        # Consulta base de guías publicadas ordenadas por fecha de creación
         guias_list = Guia.objects.filter(publicado=True).order_by('-created_at')
-        # Paginación: 6 guías por página (ajusta si lo necesitas)
+
+        # Filtrado por categoría (se omite si se selecciona "Todos")
+        category_slug = self.request.GET.get('category')
+        if category_slug and category_slug != 'all':
+            guias_list = guias_list.filter(category__slug=category_slug)
+
+        # Filtrado por búsqueda en el título
+        search_query = self.request.GET.get('search')
+        if search_query:
+            guias_list = guias_list.filter(title__icontains=search_query)
+
+        # Paginación: 6 guías por página
         paginator = Paginator(guias_list, 6)
         page = self.request.GET.get('page')
         try:
